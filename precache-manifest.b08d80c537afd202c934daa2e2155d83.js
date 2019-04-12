0e62fe0b433d7bda1556f9b8b658760d0b60eea2
Gonzalo Sobrino <gonzalosobrino6@gmail.com>
2019-04-12 20:16:29 +0200

    Updates

---
+++ precache-manifest.b08d80c537afd202c934daa2e2155d83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9fb3054e00c65841121da81c48e4fb1",
+    "url": "/martinna-web/./static/media/AirbnbCereal-Medium.d9fb3054.ttf"
+  },
+  {
+    "revision": "e4c0042b67721cf33e9d",
+    "url": "/martinna-web/./static/js/runtime~main.9ee517c6.js"
+  },
+  {
+    "revision": "5ae4e57ab64e1ed69055",
+    "url": "/martinna-web/./static/js/main.7280378c.chunk.js"
+  },
+  {
+    "revision": "89247440dc12d2501e2d",
+    "url": "/martinna-web/./static/js/2.a7c98bd9.chunk.js"
+  },
+  {
+    "revision": "5ae4e57ab64e1ed69055",
+    "url": "/martinna-web/./static/css/main.9be7254d.chunk.css"
+  },
+  {
+    "revision": "89247440dc12d2501e2d",
+    "url": "/martinna-web/./static/css/2.72040b9c.chunk.css"
+  },
+  {
+    "revision": "d1fa87fd23741a909c0bb6979f24dbe8",
+    "url": "/martinna-web/./index.html"
+  }
+];
