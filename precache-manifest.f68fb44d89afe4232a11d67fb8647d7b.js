1f80e39a4c8697534d02c605355c74d197982bd1
Gonzalo Sobrino <gonzalosobrino6@gmail.com>
2019-04-12 21:26:46 +0200

    Updates

--- precache-manifest.f68fb44d89afe4232a11d67fb8647d7b.js
+++ precache-manifest.f68fb44d89afe4232a11d67fb8647d7b.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/martinna-web/./static/js/runtime~main.9ee517c6.js"
   },
   {
-    "revision": "c46f1d9fbee7d4451837",
-    "url": "/martinna-web/./static/js/main.46f37c52.chunk.js"
+    "revision": "ac8388efaffb0dcd8057",
+    "url": "/martinna-web/./static/js/main.0e0315d2.chunk.js"
   },
   {
     "revision": "3f0e2b7c789d3f693bb9",
     "url": "/martinna-web/./static/js/2.526488fd.chunk.js"
   },
   {
-    "revision": "c46f1d9fbee7d4451837",
+    "revision": "ac8388efaffb0dcd8057",
     "url": "/martinna-web/./static/css/main.9be7254d.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/martinna-web/./static/css/2.72040b9c.chunk.css"
   },
   {
-    "revision": "d50ee791d4432f9ab23247fc1e8c004a",
+    "revision": "b8160841393e1d0fa84779e13e7d3647",
     "url": "/martinna-web/./index.html"
   }
 ];
